90756bb2a447e71c129df371c984edc4ac400d2e
100423743 <100423743@alumnos.uc3m.es>
2021-03-17 03:36:35 +0100

    Calculator fixed

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(magrittr)
 ## LOAD AND PRE-PROCESSING
 df <- read.csv("https://query.data.world/s/457fikckeqdoemry75fqfhjoqwtrxv", 
                header=TRUE, stringsAsFactors=FALSE)
-df <- df[,-c(1,3,4,8,11,13:20,22,24:30,43:92)]
+df <- df[,-c(1,3,4,8,11,14:20,22,24:30,43:92)]
 df$club_position[df$club_position %in% c("GK")] <- "GOALKEEPER"
 df$club_position[df$club_position %in% c("LCB","RCB","LB","RB","CB",
                                          "LWB","RWB")] <- "DEFENDER"
@@ -26,17 +26,15 @@ df <- df%>%dplyr::filter(club_position=="GOALKEEPER"|club_position=="DEFENDER"|
 df <- df[complete.cases(df),]
 df %<>% mutate_at(c("nationality", "club_team", "club_position"), as.factor)
 df %<>% rename(Name=name, Age=age, Height=height_cm, Weight=weight_kgs, Nationality=nationality, 
-               Overall=overall_rating, Price=value_euro, Team=club_team, Position=club_position,
+               Overall=overall_rating, Price=value_euro, Wage=wage_euro, Team=club_team, Position=club_position,
                Crossing=crossing, Finishing=finishing, Heading=heading_accuracy, 
                ShortPassing=short_passing, Volleys=volleys, Dribbling=dribbling, 
                Curve=curve, Freekick=freekick_accuracy, LongPassing=long_passing,
                Control=ball_control, Acceleration=acceleration, Speed=sprint_speed)
   
-list_choices_overall <- colnames(df[,c(7,21,11,12,13,15,17)])
-list_choices_stats <- colnames(df[,c(3,4,6,7,10:21)])
-model_price <- lm(Price ~ Overall+Age+Speed+Control+
-                    ShortPassing+Dribbling+Finishing+
-                    Height+Weight+Freekick, data=df)
+list_choices_overall <- colnames(df[,c(7,22,14,11,12,13,16,20)])
+list_choices_stats <- colnames(df[,c(3,4,6,8,11:22)])
+model_price <- lm(Price ~ Age+Wage+Speed+Control++Dribbling+ShortPassing+LongPassing,data=df)
 
 ## PANELS
 dataPanel_1 <- tabPanel("Players Rating",
@@ -75,28 +73,14 @@ dataPanel_3 <- tabPanel("Price Calculator",
                                           h3(style = "margin-left: 0px; margin-bottom: 0px;", "Age"),
                                           column(12,
                                                  sliderInput("n_age", label="", min = min(df$Age), max = max(df$Age), 
-                                                             value = min(df$Age), step = 1)
+                                                             value =29, step = 1)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Overall desired Rating"),
+                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Wage (€/year)"),
                                           column(12,
-                                                 sliderInput("n_rating", label="", min = min(df$Overall), max = max(df$Overall), 
-                                                             value = (max(df$Overall)+min(df$Overall))/2, step = 1)
-                                          )
-                                        ),
-                                        fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Height (cm)"),
-                                          column(12,
-                                                 sliderInput("n_height", label="", min = min(df$Height), max = max(df$Height), 
-                                                             value = (max(df$Height)+min(df$Height))/2, step = 0.01)
-                                          )
-                                        ),
-                                        fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Weight (kg)"),
-                                          column(12,
-                                                 sliderInput("n_weight", label="", min = min(df$Weight), max = max(df$Weight), 
-                                                             value = (max(df$Weight)+min(df$Weight))/2, step = 0.1)
+                                                 sliderInput("n_wage", label="", min = min(df$Wage), max = max(df$Wage), 
+                                                             value = (max(df$Wage)+min(df$Wage))/2, step = 0.01)
                                           )
                                         ),
                                         fluidRow(
@@ -112,33 +96,28 @@ dataPanel_3 <- tabPanel("Price Calculator",
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Short Pass"),
-                                          column(12,
-                                                 sliderInput("n_pass", label="", min = 0, max = 99, value = 50, step = 1)
-                                          )
-                                        ),
-                                        fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Freekick Accuracy"),
-                                          column(12,
-                                                 sliderInput("n_freekick", label="", min = 0, max = 99, value = 50, step = 1)
-                                          )
-                                        ),
-                                        fluidRow(
                                           h3(style = "margin-left: 0px; margin-bottom: 0px;", "Dribbling Skills"),
                                           column(12,
                                                  sliderInput("n_dribbling", label="", min = 0, max = 99, value = 50, step = 1)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Finishing Skills"),
+                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Short Pass"),
                                           column(12,
-                                                 sliderInput("n_finish", label="", min = 0, max = 99, value = 50, step = 1)
+                                                 sliderInput("n_pass", label="", min = 0, max = 99, value = 50, step = 1)
+                                          )
+                                        ),
+                                        
+                                        fluidRow(
+                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Long Pass"),
+                                          column(12,
+                                                 sliderInput("n_lpass", label="", min = 0, max = 99, value = 50, step = 1)
                                           )
                                         ),
                                       ),
                                       mainPanel(
                                         tabsetPanel(type = "tabs",
-                                                    tabPanel("Player Price Calculator", textOutput("Calculator")),
+                                                    tabPanel("Player Price Calculator", htmlOutput("Calculator")),
                                                     tabPanel("Model Summary", verbatimTextOutput("Summary"))
                                                     ) # tabsetPanel
                                         ) # mainPanel
@@ -168,7 +147,7 @@ server <- function(input, output) {
   
   # 1
   output$Overall <- plotly::renderPlotly({
-    ggplot(df, aes(x = .data[[input$select_overall]], y = Overall, color = Position)) + geom_point() + 
+    ggplot(df, aes(x = .data[[input$select_overall]], y = Overall, color = Position)) + geom_point(size=0.8) + 
       labs(colour = "Position") + scale_x_continuous(labels = scales::comma)
   })
   
@@ -178,28 +157,36 @@ server <- function(input, output) {
       geom_boxplot(alpha = 0.6) + labs(x = "Position", fill = "Position")
   })  
   
-  # 3
-  output$Calculator <- renderPrint({
-    price <- summary(model_price)$coef[1,1] + summary(model_price)$coef[2,1]*input$n_rating +
-      summary(model_price)$coef[3,1]*input$n_age + summary(model_price)$coef[4,1]*input$n_speed +
-      summary(model_price)$coef[5,1]*input$n_control + summary(model_price)$coef[6,1]*input$n_pass +
-      summary(model_price)$coef[7,1]*input$n_dribbling + summary(model_price)$coef[8,1]*input$n_finish +
-      summary(model_price)$coef[9,1]*input$n_height + summary(model_price)$coef[10,1]*input$n_weight +
-      summary(model_price)$coef[11,1]*input$n_freekick
+  # 3 Age+Wage+Speed+Control+Dribbling+ShortPassing+LongPassing
+  output$Calculator <- renderText({
+    price <- summary(model_price)$coef[1,1] + 
+      summary(model_price)$coef[2,1]*input$n_age + summary(model_price)$coef[3,1]*input$n_wage + 
+      summary(model_price)$coef[4,1]*input$n_speed + summary(model_price)$coef[5,1]*input$n_control +
+      summary(model_price)$coef[6,1]*input$n_dribbling + summary(model_price)$coef[7,1]*input$n_pass +
+      summary(model_price)$coef[8,1]*input$n_lpass
     
-    cat("You can find out how much would it cost a completely desired player by tunning its features on the right panel. \n\n")
-    cat("After doing this, the estimated buying price for a player with the following characteristics: \n\n") 
-    cat(input$n_age, "years old, \n\n")
-    cat(input$n_rating, "of overall rating, \n\n")
-    cat(input$n_height, "cm, \n\n")
-    cat(input$n_weight, "kg, \n\n")
-    cat(input$n_speed, "of speed, \n\n")
-    cat(input$n_control, "of ball control, \n\n")
-    cat(input$n_pass, "of short passing, \n\n")
-    cat(input$n_freekick, "of freekick accuracy, \n\n")
-    cat(input$n_dribbling, "of dribbling and \n\n")
-    cat(input$n_finish, "of finishing skills, \n\n")
-    cat("would be around:", price, "€")})
+    paste("Hi! In case you are the player transfer manager of a football club, you can now find out how much would it cost a desired player in terms of his stats you are most interested at the moment (tunning them on the right panel).", 
+          "<br>","<br>", 
+          "This final player price estimation has been computed from the FIFA (Fédération Internationale de Football Association) stats assigned for the 2019/20 season.",
+          "<br>","<br>", 
+          "So, after tunning these player stats, the estimated", "<b>", "buying price", "</b>", "for a player with the following characteristics:",
+          "<br>",
+          "<b>", input$n_age, "</b>", "years old,",
+          "<br>",
+          "earning", "<b>", input$n_wage, "</b>", "€ per year,",
+          "<br>",
+          "<b>", input$n_speed, "</b>", "of speed rating,",
+          "<br>",
+          "<b>", input$n_control, "</b>", "of ball control rating,",
+          "<br>",
+          "<b>", input$n_dribbling, "</b>", "of dribbling rating,",
+          "<br>",
+          "<b>", input$n_pass, "</b>", "of short pass rating,",
+          "<br>",
+          "and", "<b>", input$n_lpass, "</b>", "of finishing skills",
+          "<br>",
+          "would be around:", "<b>", price, "</b>", "€")
+  })
   
   output$Summary <- renderPrint(summary(model_price))
   
