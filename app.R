09b0656c750c5f1843f3772d03263fcaffa6b58b
100423743 <100423743@alumnos.uc3m.es>
2021-03-18 03:27:57 +0100

    Styles and forth page

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ library(shinyjs)
 library(shinythemes)
 library(plotly)
 library(magrittr)
+library(DT)
 
 ## LOAD AND PRE-PROCESSING
 df <- read.csv("https://query.data.world/s/457fikckeqdoemry75fqfhjoqwtrxv", 
@@ -34,7 +35,7 @@ df %<>% rename(Name=name, Age=age, Height=height_cm, Weight=weight_kgs, National
   
 list_choices_overall <- colnames(df[,c(7,22,14,11,12,13,16,20)])
 list_choices_stats <- colnames(df[,c(3,4,6,8,11:22)])
-list_choices_top <- colnames(df[,c(6,7,8,22,14,18,12)])
+list_choices_top <- colnames(df[,c(6,7,22,14,18,12)])
 model_price <- lm(Price ~ Age+Wage+Speed+Control++Dribbling+ShortPassing+LongPassing,data=df)
 
 ## PANELS
@@ -46,7 +47,9 @@ dataPanel_1 <- tabPanel("Players Rating",
                                         selected = 1)
                           ), mainPanel(
                             h3("Overall Rating"),
-                            plotly::plotlyOutput(outputId = "Overall")
+                            plotOutput(outputId = "Overall_plot", click="point_click"),
+                            h4("Click on a point to know the player:"),
+                            tableOutput("table_point")
                           )
                           ))
                         
@@ -71,45 +74,45 @@ dataPanel_3 <- tabPanel("Price Calculator",
                         sidebarLayout(position = "right",
                                       sidebarPanel(
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Age"),
+                                          h3(style = "margin-left: 14px; margin-bottom: 0px;", "Age"),
                                           column(12,
                                                  sliderInput("n_age", label="", min = min(df$Age), max = max(df$Age), 
                                                              value =29, step = 1)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Wage (€/year)"),
+                                          h3(style = "margin-left: 14px; margin-bottom: 0px;", "Wage (€/year)"),
                                           column(12,
                                                  sliderInput("n_wage", label="", min = min(df$Wage), max = max(df$Wage), 
                                                              value = (max(df$Wage)+min(df$Wage))/2, step = 0.01)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Sprint Speed"),
+                                          h3(style = "margin-left: 14px; margin-bottom: 0px;", "Sprint Speed"),
                                           column(12,
                                                  sliderInput("n_speed", label="", min = 0, max = 99, value = 50, step = 1)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Ball Control"),
+                                          h3(style = "margin-left: 14px; margin-bottom: 0px;", "Ball Control"),
                                           column(12,
                                                  sliderInput("n_control", label="", min = 0, max = 99, value = 50, step = 1)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Dribbling Skills"),
+                                          h3(style = "margin-left: 14px; margin-bottom: 0px;", "Dribbling Skills"),
                                           column(12,
                                                  sliderInput("n_dribbling", label="", min = 0, max = 99, value = 50, step = 1)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Short Pass"),
+                                          h3(style = "margin-left: 14px; margin-bottom: 0px;", "Short Pass"),
                                           column(12,
                                                  sliderInput("n_pass", label="", min = 0, max = 99, value = 50, step = 1)
                                           )
                                         ),
                                         fluidRow(
-                                          h3(style = "margin-left: 0px; margin-bottom: 0px;", "Long Pass"),
+                                          h3(style = "margin-left: 14px; margin-bottom: 0px;", "Long Pass"),
                                           column(12,
                                                  sliderInput("n_lpass", label="", min = 0, max = 99, value = 50, step = 1)
                                           )
@@ -126,26 +129,17 @@ dataPanel_3 <- tabPanel("Price Calculator",
                         
 
 dataPanel_4 <- tabPanel("Top Players",
-                          dashboardHeader(),
-                          dashboardSidebar(),
-                          dashboardBody(
-                            fluidRow(
-                              box(dataTableOutput("Top")),
-                              box(textInput(inputId = "n_rank",
-                                            label = "Choose a number",
-                                            value = 15)),
-                              box(selectInput("variable", "Select variable to rank", 
-                                              choices = list_choices_top))            
+                        fluidPage(
+                            mainPanel(
+                              h3("Top Players Ranking"),
+                              dataTableOutput(outputId="Top")
                             )
-                          ),
-                        mainPanel(
-                          h3("Top Players Table")
-                        )
+                          )
 )
-  
 
 # UI
 ui <- navbarPage("FIFA Football Players (Shiny Take-Away Assignment App)",
+                 theme = shinytheme("flatly"),  
                  dataPanel_1,
                  dataPanel_2,
                  dataPanel_3,
@@ -157,11 +151,15 @@ ui <- navbarPage("FIFA Football Players (Shiny Take-Away Assignment App)",
 server <- function(input, output) {
   
   # 1
-  output$Overall <- plotly::renderPlotly({
-    ggplot(df, aes(x = .data[[input$select_overall]], y = Overall, color = Position)) + geom_point(size=0.8) + 
+  output$Overall_plot <- renderPlot({
+    ggplot(df, aes(x = .data[[input$select_overall]], y = Overall, color = Position)) + geom_point() + 
       labs(colour = "Position") + scale_x_continuous(labels = scales::comma)
   })
   
+  output$table_point <- renderTable({
+    nearPoints(df, coordinfo = input$select_overall, yvar = "Overall",
+               input$point_click, maxpoints = 1)
+  })
   # 2
   output$Stats <- plotly::renderPlotly({
     ggplot(df, aes(x = Position, y = .data[[input$select_stats]], fill = Position)) +
@@ -170,17 +168,18 @@ server <- function(input, output) {
   
   # 3 
   output$Calculator <- renderText({
+    
     price <- summary(model_price)$coef[1,1] + 
       summary(model_price)$coef[2,1]*input$n_age + summary(model_price)$coef[3,1]*input$n_wage + 
       summary(model_price)$coef[4,1]*input$n_speed + summary(model_price)$coef[5,1]*input$n_control +
       summary(model_price)$coef[6,1]*input$n_dribbling + summary(model_price)$coef[7,1]*input$n_pass +
       summary(model_price)$coef[8,1]*input$n_lpass
     
-    paste("Hi! In case you are the player transfer manager of a football club, you can now find out how much would it cost a desired player in terms of his stats you are most interested at the moment (tunning them on the right panel).", 
+    paste("Hi there! In case you are the player transfer manager of a football club, you can now find out how much would it cost to sign a desired player in terms of his/her stats you are most interested in at the moment (tunning them on the right panel).", 
           "<br>","<br>", 
           "This final player price estimation has been computed from the FIFA (Fédération Internationale de Football Association) stats assigned for the 2019/20 season.",
           "<br>","<br>", 
-          "So, after tunning these player stats, the estimated", "<b>", "buying price", "</b>", "for a player with the following characteristics:",
+          "So, after tunning these player stats, the estimated", "<b>", "signing price", "</b>", "for a player with the following characteristics:",
           "<br>",
           "<b>", input$n_age, "</b>", "years old,",
           "<br>",
@@ -196,18 +195,14 @@ server <- function(input, output) {
           "<br>",
           "and", "<b>", input$n_lpass, "</b>", "of finishing skills",
           "<br>",
-          "would be around:", "<b>", price, "</b>", "€")
+          "would be around:", "<b>", round(price,2), "</b>", "€")
   })
   
   output$Summary <- renderPrint(summary(model_price))
   
   # 4
   
-  filt_df <- reactive(df[1:input$n_rank,][setorder(input$variable)])
-  output$Top <- renderDataTable({
-    datatable(filt_df()) 
-  }) 
-  
+  output$Top <- renderDataTable({df[,c(1,2,10,9,5,6,7,22,14,18,12)]})
   
 }  
 # Run the application 
